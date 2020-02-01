5e2de1660806e7a2bbf2b64d95e18b285dd92128
千年幽静 <wylshkjj@163.com>
2020-02-01 21:26:57 +0800

    创建数据库,编辑简单前端框样式

---
+++ crm/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import path,include
+from crm import views
+urlpatterns = [
+    path('',views.index,name="sales_index"),
+    path('customers',views.customer_list,name="customer_list")
+]
+
